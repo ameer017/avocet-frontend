3912c662ca9911b1d77c36bea7fb8e52e2ec668f
mac <mac@macs-MacBook-Air-2.local>
2023-10-30 14:44:05 +0100

    update day 1

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,69 +1,70 @@
-import React, {useState, useEffect} from 'react'
-import './Home.scss'
-import Loader from '../../components/loader/Loader'
-import { Link, useNavigate } from 'react-router-dom'
-import Statistics from '../statistics/Statistics'
-import Product from '../product/Product'
-import AppInner from '../app-inner/AppInner'
-import Footer from '../../components/footer/Footer'
-import Header from '../../components/header/Header'
-import Partner from '../statistics/Partner'
+import React, { useState, useEffect } from "react";
+import "./Home.scss";
+import Loader from "../../components/loader/Loader";
+import { Link, useNavigate } from "react-router-dom";
+import Statistics from "../statistics/Statistics";
+import Product from "../product/Product";
+import AppInner from "../app-inner/AppInner";
+import Footer from "../../components/footer/Footer";
+import Header from "../../components/header/Header";
+import Partner from "../statistics/Partner";
+import { ShowOnLogout } from "../../components/protect/hiddenLink";
 
 const Home = () => {
-  
-  const [isLoading, setIsLoading] = useState(true)
-  
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     setTimeout(() => {
-      setIsLoading(false)
-    }, 3000)
-  })
+      setIsLoading(false);
+    }, 3000);
+  });
 
   return (
     <>
-    {isLoading ? 
-      (<Loader/>) 
-      :
-      (
+      {isLoading ? (
+        <Loader />
+      ) : (
         <>
-        <Header/>
+          <Header />
           {/* hero section */}
-          
-            <section className=' hero'>
-              
-              <div className='hero-text'>
 
-                <h1>Welcome to Avocet.</h1>
-              
-                {/* <small>...Tokenizing Waste through Web3</small> */}
+          <section className=" hero">
+            <div className="hero-text">
+              <h1>Welcome to Avocet.</h1>
 
-                <p>Avocet is your gateway to a sustainable future. We are a waste-to-wealth platform committed to revolutionizing the way we handle waste and turning it into valuable resources. Join us on this exciting journey as we make a positive impact on the environment and create opportunities for a greener and more prosperous world..</p>
+              {/* <small>...Tokenizing Waste through Web3</small> */}
 
-                <div className='hero-buttons --flex-start'>
-                
-                  <button className='--btn --btn-secondary center'>
-                    <Link to='/register'>Register</Link>
-                    
+              <p>
+                Avocet is your gateway to a sustainable future. We are a
+                waste-to-wealth platform committed to revolutionizing the way we
+                handle waste and turning it into valuable resources. Join us on
+                this exciting journey as we make a positive impact on the
+                environment and create opportunities for a greener and more
+                prosperous world..
+              </p>
+
+              <div className="hero-buttons --flex-start">
+                <ShowOnLogout>
+                  <button className="--btn --btn-secondary center">
+                    <Link to="/register">Register</Link>
                   </button>
-              
-                </div>
-              
-                {/* <Partner/> */}
+                </ShowOnLogout>
               </div>
 
-            </section>
-            <Statistics/>
-            <hr/>
+              {/* <Partner/> */}
+            </div>
+          </section>
+          <Statistics />
+          <hr />
 
-            <Product/>
-            <AppInner/>
+          <Product />
+          <AppInner />
 
-          <Footer/>
+          <Footer />
         </>
-
       )}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

139adcad1a991f302fdd17e77e69fbd723de5454
ameer017 <rajiabdullahi907@gmail.com>
2023-12-05 11:06:32 +0100

    001_day02

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../header/Header'
 import Footer from '../footer/Footer'
+import Navbar from '../Navbar/Navbar'
 
 const Layout = ({children}) => {
   return (
     <>
       <Header/>
+      {/* <Navbar/> */}
       <div className='--pad' style={{minHeight: '80vh'}}>
           {children}
       </div>

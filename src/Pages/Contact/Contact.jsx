98bf93383e49377b9715df3c4e72c820c771689e
ameer017 <rajiabdullahi907@gmail.com>
2024-02-22 12:01:01 +0100

    contact_component_added

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import "./Contact.scss";
 
 const Contact = () => {
   return (
-    <section>Contact</section>
-  )
-}
+    <section className="__wrapper">
+      <div class="container__one">
+        <h1>Get In Touch</h1>
+        <form action="" method="post">
+          <div class="form-group">
+            <input
+              type="text"
+              id="name"
+              name="name"
+              required
+              placeholder="John Doe"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              required
+              placeholder="yourname@gmail.com"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="text"
+              id="subject"
+              name="subject"
+              required
+              placeholder="subject"
+            />
+          </div>
+          <div class="form-group">
+            <textarea
+              id="message"
+              name="message"
+              required
+              cols="10"
+              placeholder="input your message here"
+            ></textarea>
+          </div>
+          <div class="form-group">
+            <button type="submit" className="--btn --btn-block --btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;

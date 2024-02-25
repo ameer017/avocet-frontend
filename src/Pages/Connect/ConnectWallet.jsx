fd8a6ab669e51037ccc2133255a18059fafa8b6f
Smi-lin <olajideolabimpe26@gmail.com>
2024-02-25 19:49:41 +0100

    connect wallet

--- src/Pages/Connect/ConnectWallet.jsx
+++ src/Pages/Connect/ConnectWallet.jsx
@@ -1,8 +1,59 @@
-import React from "react";
+
+
+import React, { useEffect, useState } from "react";
+
+
 import "./ConnectWallet.scss";
 
 const ConnectWallet = () => {
-  return <section>ConnectWallet</section>;
+  const [activeBtn, setActiveBtn] = useState(1);
+
+
+  return (
+    <div className="connectWallet">
+      <div className="connectWallet_box">
+        <h1>Connect your wallet</h1>
+        <p className="connectWallet_box_para">
+          Connect with one of your available wallet providers or create a new
+          one
+        </p>
+        <div className="connect">
+          <div className="connect-links">
+            <img src="/src/assets/provider-1.png" />
+            <h4>Metamask</h4>
+          </div>
+          <div className="connect-links">
+            <img src="/src/assets/provider-2.png" />
+            <h4>Wallet Connect</h4>
+          </div>
+          <div className="connect-links">
+            <img src="/src/assets/provider-3.png" />
+            <h4>Wallet Link</h4>
+          </div>
+          <div className="connect-links">
+            <img src="/src/assets/provider-4.png" />
+
+            <h4>Formatic</h4>
+          </div>
+          <div className="connect-links">
+            <img src="/src/assets/provider-5.png" />
+
+            <h4>Coin Base</h4>
+          </div>
+          <div className="connect-links">
+            <img src="/src/assets/provider-6.webp" />
+
+            <h4>Joy Id</h4>
+          </div>
+          <div className="connect-links">
+            <img src="/src/assets/provider-7.png" />
+
+            <h4>Coin X</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ConnectWallet;

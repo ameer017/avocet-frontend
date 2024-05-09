20b405c30448cfbcac57a98a4d9c4be23b08b7f3
ameer017 <rajiabdullahi907@gmail.com>
2024-05-09 20:06:33 +0100

    favicon added

--- src/Components/Partners/Partner.jsx
+++ src/Components/Partners/Partner.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Partner.scss";
+import planet3r from "/src/assets/planet3r.png";
+import logo from "/src/assets/logo-no-background.png";
 
 const Partner = () => {
   return (
@@ -8,21 +10,16 @@ const Partner = () => {
         <h1>Our Partners And Clients</h1>
 
         <div className="--flex-between">
-          <img src="/src/assets/planet3r.png" alt="planet3r" title="" />
+          <img src={planet3r} alt="planet3r" title="" />
 
           <img
             src="https://www.beierrecycling.com/wp-content/themes/beierrecycling/images/logo.png"
             alt="logo-img"
           />
 
-          {/* <img
-            src="http://www.recyclepoints.com/wp-content/uploads/2017/07/Recycle-Points.png"
-            alt="RecyclePoints"
-          /> */}
-
           <img srcSet="ghrng.com/wp-content/uploads/2023/01/ghr_primary-300x86.png 300w, https://ghrng.com/wp-content/uploads/2023/01/ghr_primary-768x220.png 768w, https://ghrng.com/wp-content/uploads/2023/01/ghr_primary-1536x441.png 1536w, https://ghrng.com/wp-content/uploads/2023/01/ghr_primary-2048x588.png 2048w" />
 
-          <img src="/src/assets/logo-no-background.png" alt="" srcset="" />
+          <img src={logo} alt="" srcset="" />
         </div>
       </div>
     </section>

f6ffaf3cbe2be8aa11a9074719495180f501d17b
Bayz.ng <ayofe70@gmail.com>
2024-05-09 14:04:21 -0700

    bayo commit

--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -9,35 +9,38 @@ const Team = () => {
       <h2>Our Team</h2>
       <div className="team-members">
         <TeamMembers
-          name="Abubakr Hussein"
-          role="Project Lead"
-          image="https://images.pexels.com/photos/209728/pexels-photo-209728.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          x="https://twitter.com/kennySpec"
-          insta="https://www.instagram.com/kennyx55/"
-          linkedin="https://www.linkedin.com/in/kehinde-abubakar-029223252/"
+          name="Aliu Musa"
+          role="Co-Founder, CEO"
+          image="/src/assets/aliumusa.jpeg"
+          x="https://twitter.com/herlew99"
+          linkedin="https://www.linkedin.com/in/aliumusa/"
         />
         <TeamMembers
           name="Abdullahi Raji"
-          role="Full Stack Dev."
+          role="Co-Founder, CTO"
           image="/src/assets/IMG_20220206_143138_235.jpg"
           x="https://twitter.com/alAmeer170"
-          insta="https://www.instagram.com/a.rajiabdullahi/"
           linkedin="https://www.linkedin.com/in/raji-abdullahi-a-4598b42a5/"
         />
         <TeamMembers
-          name="Akeem Adebayo"
+          name="Abdulakeem Adebayo"
           role="Blockchain Dev."
-          image="https://images.pexels.com/photos/209728/pexels-photo-209728.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          image="/src/assets/bayo.png"
           x="https://twitter.com/AkeemAd28605307"
-          insta="https://www.instagram.com/dev_bayz?igsh=MXdhaDBwbmxxa3ltaA%3D%3D&utm_source=qr"
           linkedin="https://www.linkedin.com/in/adebayo-ademola-678530199?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+        />
+        <TeamMembers
+          name="Abubakar Kehinde"
+          role="Project Lead"
+          image="/src/assets/kenny.jpeg"
+          x="https://twitter.com/kennySpec"
+          linkedin="https://www.linkedin.com/in/kehinde-abubakar-029223252/"
         />
         <TeamMembers
           name="Rokeebah Olajide"
           role="Marketing Lead"
-          image="/src/assets/Rokeebah.jpeg"
+          image="/src/assets/roqeebah.png"
           x="https://twitter.com/Olabimpe963"
-          insta="https://www.instagram.com/adesola01__/"
           linkedin="https://www.linkedin.com/in/olajide-rokeebat-98507329a/"
         />
       </div>
